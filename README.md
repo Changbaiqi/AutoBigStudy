55c9acf2815f32f345ae18094a476b5cc19fab67
Changbaiqi <2084069833@qq.com>
2022-11-16 10:03:03 +0800

    更新README文档

--- README.md
+++ README.md
@@ -21,8 +21,10 @@
 ```java
 public class Main{
     public static void main(String[] args){
+        //这里使用AutoStudy创建一个待学习的对象，参数传入抓包获得的SSUUID
         AutoStudy autoStudy = new AutoStudy("EF3SJG6FDKGgkdD39DGJKALA");
-        autoStudy.toStudy(152);
+        autoStudy.toStudy(152); //这里填写大学习的期数，152表示2022年第25期，然后会自动大学习。
+        //学习成功会自动返回一串JSON，否则表示未学习
     }
 }
 ```
--- src/main/java/com/auto/Main.java
+++ src/main/java/com/auto/Main.java
@@ -3,8 +3,10 @@ package com.auto;
 public class Main {
 
     public static void main(String[] args) {
-        AutoStudy autoStudy = new AutoStudy("");
-        System.out.println(autoStudy.toStudy(152));
+        //这里使用AutoStudy创建一个待学习的对象，参数传入抓包获得的SSUUID
+        AutoStudy autoStudy = new AutoStudy("3FC8F3B867A95F74E22492CA94376A45447752B12A04AB3");
+        System.out.println(autoStudy.toStudy(152)); //这里填写大学习的期数，152表示2022年第25期，然后会自动大学习。
+        //学习成功会自动返回一串JSON，否则表示未学习
     }
 
 }
